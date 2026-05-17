d1eb2fd8d5a94d489cf9e25c3f5d76bbdf8550fc
Alex Monteiro <alex.monteiro@ifsc.edu.br>
2026-05-17 16:22:46 -0400

    dataset

--- generate_malicious_experiments.py
+++ generate_malicious_experiments.py
@@ -10,7 +10,7 @@ EXPERIMENTS_PER_TR = 1
 TOTAL_TRAINING_RUNS = 100
 TRAFFIC_PROFILE_DIR = Path("traffic_profiles")
 # Changed output directory to keep malicious data separate
-OUTPUT_DIR = Path("generated_malicious_experiments")
+OUTPUT_DIR = Path("dataset/generated_malicious_experiments")
 SCENARIO_SCRIPT = Path(os.environ.get("PWD", os.getcwd())) / "openran" / "my-srsproject-demo" / "multi-ue-setup" / "multi_ue_scenario.py"
 DURATION_SEC = 480  # 8 minutes
 
